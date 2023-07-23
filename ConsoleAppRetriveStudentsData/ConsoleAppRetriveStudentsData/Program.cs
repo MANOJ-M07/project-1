52dc090c1f8ce47ba7cc75e59d396fa2ce196f84
MANOJ-M07 <manojmoorthy01@gmail.com>
2023-07-23 23:41:38 +0530

    Initial Commit

---
+++ ConsoleAppRetriveStudentsData/ConsoleAppRetriveStudentsData/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRetriveStudentsData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "D:\\simplilearn\\project\\students.txt";
+            List<Student> students = new List<Student>();
+
+            
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] data = line.Split(',');
+                if (data.Length == 3)
+                {
+                    try
+                    {
+                        Student student = new Student
+                        {
+                            Name = data[0].Trim(),
+                            Age = int.Parse(data[1].Trim()),
+                            Grade = data[2].Trim()
+                        };
+                        students.Add(student);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine($"Invalid data format: {line}. Skipping this line.");
+                        Console.WriteLine("\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Invalid data format: {line}. Skipping this line.");
+                    Console.WriteLine("\n");
+                }
+            }
+   
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, Grade: {student.Grade}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppRetriveStudentsData/ConsoleAppRetriveStudentsData/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppRetriveStudentsData
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Grade { get; set; }
+    }
+}
